ea51b489e8596eb8f613f8f4852f60788141f34f
AwkwardDark <81935127+AwkwardDark@users.noreply.github.com>
2024-06-18 17:03:28 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -6,55 +6,57 @@
 /*   By: pajimene <pajimene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 12:39:27 by pajimene          #+#    #+#             */
-/*   Updated: 2024/06/17 20:01:11 by pajimene         ###   ########.fr       */
+/*   Updated: 2024/06/18 16:57:58 by pajimene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	ft_child_process(char **argv, char **envp, int *fd)
+void	ft_child_process(char **argv, char **envp, t_pipex *p)
 {
 	int	infile;
 
-	close(fd[0]);
+	// close(fd[0]);
 	infile = open(argv[1], O_RDONLY, 0777);
 	if (infile == -1)
 		ft_error();
 	dup2(infile, STDIN_FILENO);
-	dup2(fd[1], STDOUT_FILENO);
+	dup2(p->fd[1], STDOUT_FILENO);
+	close(p->fd[0]);
+	ft_exec_cmd(argv[2], envp);
 }
 
-void	ft_parent_process(char **argv, char **envp, int *fd)
+void	ft_parent_process(char **argv, char **envp, t_pipex *p)
 {
 	int	outfile;
 
-	close(fd[1]);
-	outfile = read(fd[0], O_RDONLY, 0777);
+	// close(fd[1]);
+	outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (outfile == -1)
 		ft_error();
+	dup2(p->fd[0], STDIN_FILENO);
 	dup2(outfile, STDOUT_FILENO);
-	dup2(fd[0], STDIN_FILENO);
+	close(p->fd[1]);
+	ft_exec_cmd(argv[3], envp);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	(void)envp;
-	(void)argv;
-	int		fd[2];
+	t_pipex	p;
 	//fd[0] -> read
 	//fd[1] -> write
-	pid_t	pid;
 
 	if (argc == 5)
 	{
-		if (pipe(fd) == -1)
+		if (pipe(p.fd) == -1)
 			return (ft_error());
-		pid = fork();
-		if (pid == -1)
+		p.pid = fork();
+		if (p.pid == -1)
 			return (ft_error());
-		if (pid == 0)
-			ft_child_process(argv, envp, fd);
-		ft_parent_process(argv, envp, fd);
+		if (p.pid == 0)
+			ft_child_process(argv, envp, &p);
+		waitpid(p.pid, NULL, 0);
+		ft_parent_process(argv, envp, &p);
 		return (0);
 	}
 	else
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: pajimene <pajimene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 12:31:58 by pajimene          #+#    #+#             */
-/*   Updated: 2024/06/17 19:21:43 by pajimene         ###   ########.fr       */
+/*   Updated: 2024/06/18 16:55:59 by pajimene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,25 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 
+typedef struct s_pipex
+{
+	pid_t	pid;
+	int		fd[2];
+	char	*path;
+	char	**cmd;
+}	t_pipex;
+
 //String Utils
 void	ft_putstr_fd(char *s, int fd);
+void	*ft_calloc(int number, int size);
+char	**ft_split(char const *s, char c);
+void	ft_free(char **tab);
+int		ft_strncmp(char *s1, char *s2, int n);
+char	*ft_strjoin(char  *s1, char  *s2);
+void	ft_free(char **tab);
 
 //Pipex Utils
-int	ft_error(void);
+void	ft_exec_cmd(char *cmd, char **envp);
+int		ft_error(void);
 
 #endif
